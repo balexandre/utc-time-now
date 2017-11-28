354d6dba7e8006efbfa90ef7fc9ae902ff8b8bbd
Bruno Alexandre <bruno.in.dk@gmail.com>
2017-11-28 21:51:55 +0100

    added ip and morgan

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
-// server.js
-
-// BASE SETUP
+// server.js BASE SETUP
 // =============================================================================
-var moment     = require('moment');
-var express    = require('express');
-var app        = express();
+var moment = require('moment');
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-// ROUTES 
+// ROUTES
 // =============================================================================
 var router = express.Router();
 
-router.get('/', function (req, res) {
+router.use(function (req, res, next) {
+    console.log('Something is happening.');
+    next();
+});
+
+router.all('/time', function (req, res) {
     const time = moment.utc();
+    const ip = req
+        .headers['x-forwarded-for']
+        .split(',')
+        .pop() || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket
+        ? req.connection.socket.remoteAddress
+        : null);
+
     res.json({
         iso: time.toISOString(),
-        date: time.format('YYYY-MM-DD HH:mm:ss')
+        date: time.format('YYYY-MM-DD HH:mm:ss'),
+        ip
     });
 });
 
-app.use('/time', router);
+// REGISTER OUR ROUTES
+// =============================================================================
+app.use('/', router);
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on http://localhost:' + port);
+console.log('API can be called on port ' + port);
