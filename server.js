27ea8a44846f0d74b9ef840e1a28c19745e2a8ab
Bruno Alexandre <bruno.in.dk@gmail.com>
2017-11-28 20:56:50 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+var moment     = require('moment');
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+// ROUTES 
+// =============================================================================
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    const time = moment.utc();
+    res.json({
+        iso: time.toISOString(),
+        date: time.format('DD-MM-YYYY HH:mm:ss')
+    });
+});
+
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on http://localhost:' + port);
